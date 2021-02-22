7dead9ae9b1e0ce551f0b9b16718f3469f2adfcb
Amos <alphatrl1997@gmail.com>
2021-02-22 22:11:50 +0800

    feat: add nginx test
    - add nginx test
    - add debugging

--- src/networking/CheckServerStatus.ts
+++ src/networking/CheckServerStatus.ts
@@ -10,21 +10,39 @@ const CheckServerStatus = async (): Promise<Record<string, unknown>> => {
   const pulse = ':8001/testpoint';
   const agent = new https.Agent({ rejectUnauthorized: false });
 
+  const isNginxAlive = await axios
+    .get(`http://${serverURL}`, { timeout: 10000 })
+    .then(() => {
+      return { isAlive: true };
+    })
+    .catch((error: AxiosError) => {
+      console.log('---NGINX---');
+      console.log(`${error.name}: ${error.code}`);
+      console.log('[MSG]', error.message);
+      return { isAlive: false, code: error.code };
+    });
+
   const isCatAlive = await axios
     .get(`http://${serverURL}${catSystem}`, { timeout: 10000 })
     .then(() => {
       return { isAlive: true };
     })
     .catch((error: AxiosError) => {
+      console.log('---CAT---');
+      console.log(`${error.name}: ${error.code}`);
+      console.log('[MSG]', error.message);
       return { isAlive: false, code: error.code };
     });
 
   const isOSTicketAlive = await axios
-    .get(`https://${serverURL}`, { timeout: 10000, httpsAgent: agent })
+    .get(`https://${serverURL}`, { timeout: 10000 })
     .then(() => {
       return { isAlive: true };
     })
     .catch((error: AxiosError) => {
+      console.log('---Ticketing---');
+      console.log(`${error.name}: ${error.code}`);
+      console.log('[MSG]', error.message);
       return { isAlive: false, code: error.code };
     });
 
@@ -36,10 +54,14 @@ const CheckServerStatus = async (): Promise<Record<string, unknown>> => {
       return { isAlive: true };
     })
     .catch((error: AxiosError) => {
+      console.log('---Pulse---');
+      console.log(`${error.name}: ${error.code}`);
+      console.log('[MSG]', error.message);
       return { isAlive: false, code: error.code };
     });
 
   return {
+    nginx: isNginxAlive,
     cat: isCatAlive,
     osticket: isOSTicketAlive,
     pulse: isPulseAlive,
