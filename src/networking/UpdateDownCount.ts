3ed393441dab1967162302fe0cdc3c55e02bfdfd
Amos <alphatrl1997@gmail.com>
2021-03-16 20:46:05 +0800

    fix: count reporting incorrect downtime

--- src/networking/UpdateDownCount.ts
+++ src/networking/UpdateDownCount.ts
@@ -26,10 +26,10 @@ const UpdateDownCount = (
   let pulseCount = counter?.pulse ? parseInt(`${counter.pulse}`) : 0;
   let ticketCount = counter?.osticket ? parseInt(`${counter.osticket}`) : 0;
 
-  nginxCount = data.nginx.isAlive ? 0 : nginxCount++;
-  catCount = data.cat.isAlive ? 0 : catCount++;
-  pulseCount = data.pulse.isAlive ? 0 : pulseCount++;
-  ticketCount = data.osticket.isAlive ? 0 : ticketCount++;
+  nginxCount = data.nginx.isAlive ? 0 : nginxCount + 1;
+  catCount = data.cat.isAlive ? 0 : catCount + 1;
+  pulseCount = data.pulse.isAlive ? 0 : pulseCount + 1;
+  ticketCount = data.osticket.isAlive ? 0 : ticketCount + 1;
 
   return {
     nginx: nginxCount,
