7fd8dcaf004b8bd605e6ad230e93db2ee3937ab7
Amos <alphatrl1997@gmail.com>
2021-03-16 21:02:05 +0800

    fix: fix rebase wrong files

--- email-server/src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as StatusTable } from './Status/StatusTable';
-export { default as StatusColor } from './Status/StatusColor';
-export { default as StatusTime } from './Status/StatusTime';
-export { default as StatusLegend } from './Status/StatusLegend';
-export { default as Footer } from './Footer';
-
